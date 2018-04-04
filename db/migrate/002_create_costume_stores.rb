1ba5b084cd60086990c93c3cb27e99f6497ad9c9
Jawann <jawann@gmail.com>
2018-04-04 00:33:16 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-      t.bool :still_in_business
+      t.boolean :still_in_business
     end
   end
 end
