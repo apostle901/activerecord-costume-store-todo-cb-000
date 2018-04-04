fc0efb6fc0d919aee36686950bed42fb15025478
Jawann <jawann@gmail.com>
2018-04-04 00:19:15 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,7 +12,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
     create_table :costumes do |t|
       t.string :name
       t.float :price
-      t.string :url
+      t.string :image_url
     end
   end
 end
